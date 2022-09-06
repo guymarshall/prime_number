e40caba6c95a6923217e864c76bb0602debbdde1
Guy Marshall <guymarshall9@gmail.com>
2022-09-06 22:42:49 +0100

    Remove comments

--- app2.py
+++ app2.py
@@ -1,29 +1,3 @@
-# import math
-
-
-# def is_prime_number(number):
-#     for i in range(2, math.ceil(math.sqrt(number)) + 1):
-#         if number % i == 0:
-#             return False
-#     return True
-
-
-# def main():
-#     user_input = int(input("Enter a positive integer: "))
-
-#     prime_number_count = 0
-#     number = 2
-#     while prime_number_count < user_input:
-#         is_prime = is_prime_number(number)
-#         if is_prime:
-#             prime_number_count += 1
-#             print(f"{prime_number_count}: {number}")
-#         number += 1
-
-
-# if __name__ == "__main__":
-#     main()
-
 def generate_list(number):
     numbers = []
 
